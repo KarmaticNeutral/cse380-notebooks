2fa7ac2ace19dcdf2f04032b9fb445a6a9a7c42c
Timothy Taylor <karmaticneutral@gmail.com>
2021-02-08 12:30:15 -0700

    Structure for W06 and fix to PP5 conjecture

--- W05/PP5.py
+++ W05/PP5.py
@@ -1,25 +1,37 @@
-from numba import jit
+def choose(n, k):
+    p = 1    
+    for i in range(1, min(k, n - k) + 1):
+        p *= n
+        p //= i
+        n -= 1
+    return p
 
-@jit
-def num_2s_prime_fac(n):
-    x = n
-    num_2s = 0
-    while x % 2 == 0:
-        num_2s = num_2s + 1
-        x = x / 2
-    return num_2s
+def num_2s_prime_fac(x):
+    #print(x)
+    prime = 2
+    count = 0 
+    val = prime
+    while (True): 
+        a = x // val
+        b = (x - 1) // val
+        val *= prime
+        if (a - b): 
+            count += (a - b)
+        else: 
+            break
+    return int(count)
 
-@jit
-def conjecture(m):
-    o = m / 2 ** num_2s_prime_fac(m)
-    return o % 2 == 1 and o > 0
 
-@jit
+def test_conjecture(n):
+    a = bin(n).count("1") 
+    b = num_2s_prime_fac(choose(2*n, n))
+    return a == b
+
+
 def verify_block(current_number, block_size):
     for n in range(current_number, current_number + block_size):
-        if not conjecture(n):
+        if not test_conjecture(n):
             print("FALSE AT ", n)
-            break
 
 from datetime import datetime, timedelta
 
@@ -30,7 +42,7 @@ end_date = datetime.now() + timedelta(hours = 24)
 print("Start Time: ", start_date)
 print("Intended End Time: ", end_date)
 
-block_size = 10000000
+block_size = 100
 current_number = 1
 
 while(current_date < end_date):
